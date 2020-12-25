2f3091a431dd73731a24d39933b7ab4672624d6b
SoCuul <socuul@outlook.com>
2020-12-24 17:05:27 -0800

    Fix the help command

--- commands/help.js
+++ commands/help.js
@@ -1,15 +1,12 @@
 exports.run = async (client, message, args) => {
 
-        const Discord = require("discord.js");
-        const tick = '`'
-        const embed = new Discord.MessageEmbed()
-        .setColor(client.config.embedColor)
-        .setTitle(client.config.botName)
-        .setDescription(`${tick}For more help, contact <@${client.config.ownerID}>.`)
-        .addFields(
-            { name: `\u200b\n${client.config.giveawayEmoji} **Giveaway Commands**', value: "**!gmainstart [duration] [winners] [prize]**: ${tick}Starts a main giveaway${tick}\n**!gstart [channel] [duration] [winners] [prize]**: ${tick}Starts a giveaway${tick}\n**!greroll [message id]**: ${tick}Rerolls a giveaway winner${tick}\n**!geditwinners [message id] [new value]**: ${tick}Changes the giveaway winner amount.${tick}\n**!geditprize [message id] [new value]**: ${tick}Changes the giveaway prize.${tick}\n**!gend [message id]**: ${tick}Ends a giveaway early${tick}\n**!gdelete [message id]**: ${tick}Deletes a giveaway.${tick}\n\u200b` },
-            { name: `➡️ **Other Commands**', value: "**!grestartbot**: ${tick}Restarts the bot's process${tick}` },)
+    const Discord = require("discord.js");
+    const tick = '`'
+    const embed = new Discord.MessageEmbed()
+    .setColor(client.config.embedColor)
+    .setTitle(client.config.botName)
+    .setDescription(`For more help, contact <@${client.config.ownerID}>\n\n${client.config.giveawayEmoji} **Commands**\n**${client.config.prefix}mainstart [duration] [winners] [prize]**: ${tick}Starts a main giveaway${tick}\n**${client.config.prefix}start [channel] [duration] [winners] [prize]**: ${tick}Starts a giveaway${tick}\n**${client.config.prefix}reroll [message id]**: ${tick}Rerolls a giveaway winner${tick}\n**${client.config.prefix}editwinners [message id] [new value]**: ${tick}Changes the giveaway winner amount.${tick}\n**${client.config.prefix}editprize [message id] [new value]**: ${tick}Changes the giveaway prize.${tick}\n**${client.config.prefix}end [message id]**: ${tick}Ends a giveaway early${tick}\n**${client.config.prefix}delete [message id]**: ${tick}Deletes a giveaway.${tick}`)
 
-        message.channel.send(embed)
+    message.channel.send(embed)
 
 };
